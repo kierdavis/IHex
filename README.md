9baa18ab050fb3646abf39ab60e4075628d0e88c
Kier Davis <kierdavis@gmail.com>
2011-07-02 13:33:21 +0100

    Imported existing files

---
+++ README.md
@@ -0,0 +1,15 @@
+IHex
+=====
+
+A Python library for reading and writing Intel Hex files.
+
+License
+-------
+This work is hereby released into the Public Domain.
+
+To view a copy of the public domain dedication, visit
+[http://creativecommons.org/licenses/publicdomain][publicdomain] or send
+a letter to Creative Commons, 559 Nathan Abbott Way, Stanford, California 94305, USA.
+
+[publicdomain]:       http://creativecommons.org/licenses/publicdomain
+                      "Public Domain License"
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+  name = "IHex",
+  version = "0.1.0",
+  
+  py_modules = ["ihex"],
+  
+  author = "Kier Davis",
+  author_email = "kierdavis@gmail.com",
+  description = "A Python library for reading and writing Intel Hex files",
+  url = "https://github.com/kierdavis/IHex",
+  keywords = "intel hex binary files",
+  classifiers = ["Programming Language :: Python", "Operating System :: OS Independent", "Topic :: Software Development :: Libraries :: Python Modules"],
+)
