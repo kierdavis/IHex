7aba4e3da1e7a48402a9c72509d5a2359b94df90
Jiri Pinkava <j-pi@seznam.cz>
2013-01-11 08:52:19 +0100

    clean-up whitespaces, add license to egg

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ from setuptools import setup
 
 setup(
   name = "IHex",
-  version = "0.1.1",
-  
+  version = "0.1.2",
+
   py_modules = ["ihex"],
-  
+
   author = "Kier Davis",
   author_email = "kierdavis@gmail.com",
   description = "A Python library for reading and writing Intel Hex files",
   url = "https://github.com/kierdavis/IHex",
   keywords = "intel hex binary files",
-  classifiers = ["Programming Language :: Python", "Operating System :: OS Independent", "Topic :: Software Development :: Libraries :: Python Modules"],
+  classifiers = ["Programming Language :: Python",
+      "License :: Public Domain",
+      "Operating System :: OS Independent",
+      "Topic :: Software Development :: Libraries :: Python Modules"],
 )
